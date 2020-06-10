bd74d164c565bf5d81773ffa0ee0d82bee2a8aae
Megan Comeau <mlcomeau99@gmail.com>
2020-06-10 02:10:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,5 +1,5 @@
-class AddActiveToStudents < ActiveRecord::Migration 
-  def change 
+class AddActiveToStudents < ActiveRecord::Migration
+  def change
     def add_column :students, :active, :boolean, default: false
-  end 
-end 
+  end
+end
