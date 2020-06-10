db7e908a9c49e152a54dfab0e9c5fc15ff64394b
Megan Comeau <mlcomeau99@gmail.com>
2020-06-10 02:25:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def activate
-    set_student 
+    set_student
     @student.active = !@student.active
     @student.save
     redirect_to student_path
